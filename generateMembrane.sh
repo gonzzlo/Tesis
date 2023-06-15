cc63ca801c8d77faac741972ded07c4eb1fc1573
Gonzalo Espinoza <40776922+gonzzlo@users.noreply.github.com>
2023-06-15 18:54:57 -0400

    Update generateMembrane.sh

--- generateMembrane.sh
+++ generateMembrane.sh
@@ -1,2 +1,4 @@
 #Create a Membrane with simple structure from Martini2
 gmx insert-molecules -ci DPPC-em.gro -box 20 20 20 -nmol 1024 -radius 0.21 -try 500 -o 1024_noW.gro
+#Create a Minimization
+gmx grompp -f minimization.mdp -c 1024_noW.gro -p dppc.top -o dppc-min-init.tpr
